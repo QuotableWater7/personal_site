54d5a3b2b6a207ec5c1320f3a9c06e0ee1e4bab2
JT Bowler <jbowler2400@gmail.com>
2014-12-03 09:12:30 -0800

    Update blog to render posts when params are provided

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,7 +1,17 @@
 class BlogController < ApplicationController
 
+  layout 'blog_layout'
+
   def index
-    @query_string = request.query_string
+    query_string = request.query_string
+    p query_string
+
+    if query_string.empty?
+      render :index
+    else
+      post = (query_string.match /post=(.*)/).captures[0]
+      render post
+    end
   end
 
 end
