bca5cc6b7a5ec34e5decb6abe20e89938125e633
JT <jbowler2400@gmail.com>
2014-12-11 20:08:13 -0800

    Adding to framework code

--- app/assets/javascripts/blog/framework.js
+++ app/assets/javascripts/blog/framework.js
@@ -8,13 +8,32 @@
   };
 
   JT.Model = function () {
+    if ('blah') { console.log('weee'); }
 
+    return {
+      alert: function (text) { alert(text); }
+    };
   };
 
-  var extend = function () {
+  var extend = function (options) {
     var parent = this;
     var child;
 
+    if (options && _.has(options, 'constructor')) { child = options.constructor; console.log('using constructor'); }
+    else { child = function () { return parent.apply(this, arguments); } }
 
+    _.extend(child, parent);
+
+    return child;
   }
+
+  JT.Model.extend = extend;
+
+  var BlahModel = JT.Model.extend({
+    newAlert: function (text) { alert(text + ' yeah.'); }
+  });
+
+  var my_model = new BlahModel();
+  // console.log(my_model);
+  // my_model.newAlert();
 })();
